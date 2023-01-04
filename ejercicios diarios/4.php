f1615712f79080614960c4860d71bc7128d88b69
diegoteos <diegoteos@hotmail.com>
2023-01-04 16:14:49 -0600

    ejercicio 73

---
+++ ejercicios diarios/4.php
@@ -0,0 +1,11 @@
+<?php
+
+echo "This problem was asked by Google.
+Given the head of a singly linked list, reverse it in-place. <br/>
+Este problema fue solicitado por Google.
+Dada la cabeza de una lista enlazada individualmente, inviértala en el lugar. <br/>
+";
+
+
+
+
