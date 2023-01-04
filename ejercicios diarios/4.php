4b5968a38eeb3612dc8e918296d0c4d7b1ae9b8a
diegoteos <diegoteos@hotmail.com>
2023-01-04 16:36:35 -0600

    invertir lista

--- ejercicios diarios/4.php
+++ ejercicios diarios/4.php
@@ -5,7 +5,27 @@ Given the head of a singly linked list, reverse it in-place. <br/>
 Este problema fue solicitado por Google.
 Dada la cabeza de una lista enlazada individualmente, inviértala en el lugar. <br/>
 ";
+echo '<br/>';
+echo '<br/>';
 
 
+$frutas = array('manzana', 'mango', 'piña', 'aguacate', 'melon', 'uva');
+print_r($frutas);
+echo '<br/>';
+echo '<br/>';
+
+$contador = count($frutas);
+
+echo '<br/>';
+// var_dump($contador);
+$frutaRevertida = array();
+for ($i = $contador - 1 ; $i >= 0; $i--) { 
+    echo $frutas[$i] . '<br/>';
+    array_push($frutaRevertida, $frutas[$i]); 
+}
+echo '<br/>';
+echo '<br/>';
+echo '<br/>';
+print_r($frutaRevertida);
 
 
