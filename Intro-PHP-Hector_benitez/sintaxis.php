eec38736ba138d3e5c2b28437e94765abdac7745
diegoteos <diegoteos@hotmail.com>
2023-01-18 20:34:33 -0600

    poo

--- Intro-PHP-Hector_benitez/sintaxis.php
+++ Intro-PHP-Hector_benitez/sintaxis.php
@@ -1,3 +1,25 @@
 <?php
 
-var_dump($array = ['uno ' => 'uno']) ;
+$array = ["hola"];
+
+$jobs = [
+    [
+
+        'title' => "PHP Developer",
+    ],
+    [
+
+        'title'  => "Python Dev",
+    ],
+    [
+
+        'title' => "Devops"
+    ]
+];
+echo '<pre>'; 
+print_r($jobs);
+
+$a = 32+3;
+$b = (3)(2+3);
+echo $a;
+echo $b;
---
+++ pruebas-unitarias/documentacion/comentarios.php
@@ -0,0 +1,12 @@
+<?php
+
+/** 
+ * Retorna la suma de dos numeros
+ * @param int $a;
+ * @param int $b;
+ */
+
+function add($a, $b)
+{
+    return  $a + $b;
+}
