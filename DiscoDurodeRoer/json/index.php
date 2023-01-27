8580dd7224d98849f7d6653113431ac841926247
diegoteos <diegoteos@hotmail.com>
2023-01-26 19:48:05 -0600

    json

---
+++ DiscoDurodeRoer/json/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <form action="guardar.php" method="post">
+        <label for="nombre">Nombre</label>
+        <input type="text" name="nombre" id="nombre">
+        <label for="apellido">Apellido</label>
+        <input type="text" name="apellido" id="apellido">
+        <label for="edad">Edad</label>
+        <input type="text" name="edad" id="edad">
+        <input type="submit" value="Guardar">
+    </form>
+</body>
+
+</html>
