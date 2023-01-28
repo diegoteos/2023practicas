9dad599a3ecc0cb9e0bca6bbfb0220a644198843
diegoteos <diegoteos@hotmail.com>
2023-01-27 21:38:39 -0600

    ejercicios basicos

--- DiscoDurodeRoer/ejercicios_basicos/1.php
+++ DiscoDurodeRoer/ejercicios_basicos/1.php
@@ -1,17 +1,30 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>HTML con PHP</title>
 </head>
+
 <body>
     <?php
-        $ruta = "https://media.istockphoto.com/id/177794699/es/foto/lobo-gris-retrato.jpg?s=612x612&w=0&k=20&c=qAiwRte1ZceIxyok1IuzX88-ppmof9Ci9Aq7LZQCZ84=";
-        echo "Hola mundo";
+    $ruta = "https://media.istockphoto.com/id/177794699/es/foto/lobo-gris-retrato.jpg?s=612x612&w=0&k=20&c=qAiwRte1ZceIxyok1IuzX88-ppmof9Ci9Aq7LZQCZ84=";
+    echo "Hola mundo <br />";
     ?>
 
     <img src="<?php echo $ruta; ?>" alt="">
+    <br>
+
+    <select name="" id="">
+        <?php
+
+for ($i=1; $i <= 10; $i++) { 
+    echo '<option value="1">' . $i . '</option><br />';
+}
+        ?>
+    </select>
 </body>
+
 </html>
