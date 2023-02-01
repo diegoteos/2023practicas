da6eb3719231e74e98f2d351572fd24169f52cb3
diegoteos <diegoteos@hotmail.com>
2023-01-31 23:19:16 -0600

    alumos develoteca

--- ejercicios diarios/str_replace.php
+++ ejercicios diarios/str_replace.php
@@ -1,8 +1,12 @@
 
 <?php 
 
-$saludo = "Hola mundo";
+$saludo = "Hola mundo \n";
+$dos = "hola";
 
 $saludo = str_replace('Hola ', 'Saludos a todo el ', $saludo);
 
 echo $saludo;
+echo $dos;
+
+var_dump($saludo);
