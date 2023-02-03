5d078d6bfdb6ba424584c74a94b3e94ad1b68841
diegoteos <diegoteos@hotmail.com>
2023-02-02 23:14:45 -0600

    api ip

--- ejercicios diarios/api/api.php
+++ ejercicios diarios/api/api.php
@@ -1,4 +1,18 @@
 <?php
-    $ip = file_get_contents('https://api.ipify.org');
-    echo "My public IP address is: " . $ip;
-?>
+
+
+$url = 'https://nordvpn.com/wp-admin/admin-ajax.php?action=get_user_info_data&ip=' . $_SERVER['REMOTE_ADDR'];
+
+$info = file_get_contents($url);
+
+$info = json_decode($info, true);
+echo '<pre>';
+print_r($info);
+
+if ($info['isp'] != 'Unknown') {
+    $ip = $info['ip'];
+    $isp = $info['isp'];
+    $pais = $info['country'];
+    $region = $info['region'];
+    echo "La IP es: $ip el proveedor de servicio es: $isp del pais $pais aproximadamente en el departamento de $region";
+}
