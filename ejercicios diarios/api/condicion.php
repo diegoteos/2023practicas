cddd98f6d70518fccfd9855f78e40180b61ed8b7
diegoteos <diegoteos@hotmail.com>
2023-02-08 21:42:06 -0600

    api

--- ejercicios diarios/api/condicion.php
+++ ejercicios diarios/api/condicion.php
@@ -5,6 +5,10 @@ include_once 'api.php';
 if ($pais == 'Guatemala'){
     echo "Bienvenido por que eres de $pais <br/>";
     echo "Tu IP es: $ip <br/>";
+    echo "ISP: $isp <br/>";
+    echo "Pais: $pais <br/>";
+    echo "Area aproximada de activacion: $region <br/>";
+
 } else {
     echo "No puedes ingresar a este sitio porque nos visitas de: $pais <br/>";
 }
