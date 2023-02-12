61cdba8ee0c10716badd5d789faaf88064b5a96d
diegoteos <diegoteos@hotmail.com>
2023-02-11 22:37:54 -0600

    formularios

---
+++ DiscoDurodeRoer/formulario_radio/recibe.php
@@ -0,0 +1,28 @@
+<?php
+
+session_start();
+
+
+$figura = $_POST['figura'];
+$resultado = 0;
+
+
+switch ($figura) {
+    case 'circulo':
+        $radio = $_POST['radio'];
+        $resultado = ($radio ** 2) * pi(); 
+        break;
+
+    case 'cuadrado':
+        $lado = $_POST['lado'];
+        $resultado = $lado * $lado;
+        break;
+
+    case 'triangulo':
+        $base = $_POST['base'];
+        $altura = $_POST['altura'];
+        $resultado = ($base * $altura) / 2;
+        break;
+}
+
+$_SESSION['resultado'] = $resultado;
