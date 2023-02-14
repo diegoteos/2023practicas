38918fa792778722a5e974c8b449f3230fc87f73
diegoteos <diegoteos@hotmail.com>
2023-02-13 23:36:02 -0600

    poo

---
+++ carlos_alfaro/poo/saiyajin.php
@@ -0,0 +1,26 @@
+<?php
+
+class Saiyajin
+{
+
+    // si queremos un tipado estricto podemos definirlo pero no es obligatorio
+    public string $nombre = 'Goku';
+    public int $nivel_pelea = 1000;
+
+
+    // podemos tipar la respuesta de la funcion, agregando : y definiendo el tipo de dato
+    public function Saludar(): string
+    {
+        return "Hola soy " . $this->nombre;
+    }
+
+    public function NivelDePelea()
+    {
+        return $this->nombre . ' tiene un nivel de pelea de ' . $this->nivel_pelea;
+    }
+}
+
+$goku = new Saiyajin();
+// var_dump($goku);  muestra que es un objeto 
+echo $goku->NivelDePelea();
+
