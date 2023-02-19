5018c9c2f843fdfbd7707d337a6bcb8bca6efde2
diegoteos <diegoteos@hotmail.com>
2023-02-18 23:03:11 -0600

    ajax

--- ejercicios diarios/ajax-php/app.js
+++ ejercicios diarios/ajax-php/app.js
@@ -1,13 +1,24 @@
-$(function(){
+$(function () {
     console.log('jQuery is working');
-    $('#search').keyup(function(){
+    $('#search').keyup(function () {
         let search = $('#search').val();
         $.ajax({
             url: 'task-search.php',
             type: 'POST',
-            data: {search},
-            success: function(response){
-                let task = JSON.parse(response);
+            data: { search },
+            success: function (response) {
+                let tasks = JSON.parse(response);
+                let template = '';
+
+                tasks.forEach(task=>{
+                    template += `
+                    <li>
+                    ${task.placa}
+                    ${task.id_registro}
+                    </li>
+                    `
+                })
+                $('#container').html(template);
             }
         })
     });
