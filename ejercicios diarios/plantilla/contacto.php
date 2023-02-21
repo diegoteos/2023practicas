2f39ea911061f32aaacb6338f806298e9c3a4f9e
diegoteos <diegoteos@hotmail.com>
2023-02-20 21:41:23 -0600

    consagrado

---
+++ ejercicios diarios/plantilla/contacto.php
@@ -0,0 +1,147 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<title>Portfolio Responsive HTML Template</title>
+	<meta charset="utf-8">
+	<meta content="width=device-width, initial-scale=1.0" name="viewport">
+	<meta name="description" content="Portfolio">
+	<meta name="keywords"
+		content="Creative, minimal, Portfolio, cv, business, Resume, Information, personal portfolio, Professional">
+	<meta name="author" content="hsoft">
+	<meta name="MobileOptimized" content="320">
+	<!--Srart Style -->
+	<link rel="stylesheet" type="text/css"
+		href="http://kamleshyadav.com/html/portfolio/html/bootstrap5/white_version_typed/css/animate.css">
+	<link rel="stylesheet" type="text/css"
+		href="http://kamleshyadav.com/html/portfolio/html/bootstrap5/white_version_typed/css/fonts.css">
+	<link rel="stylesheet" type="text/css"
+		href="http://kamleshyadav.com/html/portfolio/html/bootstrap5/white_version_typed/css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css"
+		href="http://kamleshyadav.com/html/portfolio/html/bootstrap5/white_version_typed/css/font-awesome.css">
+	<link rel="stylesheet" type="text/css"
+		href="http://kamleshyadav.com/html/portfolio/html/bootstrap5/white_version_typed/css/magnific-popup.css">
+	<link rel="stylesheet" type="text/css"
+		href="http://kamleshyadav.com/html/portfolio/html/bootstrap5/white_version_typed/css/owl.carousel.min.css">
+	<link rel="stylesheet" type="text/css"
+		href="http://kamleshyadav.com/html/portfolio/html/bootstrap5/white_version_typed/css/owl.theme.default.min.css">
+	<link rel="stylesheet" type="text/css"
+		href="http://kamleshyadav.com/html/portfolio/html/bootstrap5/white_version_typed/css/style.css">
+	<!-- Favicon Link -->
+	<!-- <link rel="shortcut icon" type="image/png"
+		href="http://kamleshyadav.com/html/portfolio/html/bootstrap5/white_version_typed/images/header/favicon.png"> -->
+</head>
+
+<body>
+
+	<!--Contact Wrapper Start-->
+	<div class="	">
+		<div class="prt_close_wrapper">
+			<img src="http://kamleshyadav.com/html/portfolio/html/bootstrap5/white_version_typed/images/header/right_arrow.png"
+				alt="Close" class="prt_close">
+		</div>
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+					<div class="prt_heading_wrapper">
+						<div class="prt_heading">
+							<br><br>
+							<h1>get in touch</h1>
+							<div class="typed-strings">
+								<p>contact us</p>
+								<p>message us</p>
+							</div>
+							<p class="typed"></p>
+						</div>
+					</div>
+				</div>
+				<div class="col-xl-6 col-lg-6 col-md-12 col-sm-12 col-xs-12">
+					<div class="prt_contact_info">
+						<h1>How we can help you ?</h1>
+						<div class="prt_contact_form">
+							<div class="row">
+								<div class="col-12">
+									<form>
+										<div class="row">
+											<div class="col-xl-6 col-lg-6 col-md-6 col-sm-6 col-xs-12">
+												<input type="text" name="first_name" placeholder="Your Name"
+													class="require">
+											</div>
+											<div class="col-xl-6 col-lg-6 col-md-6 col-sm-6 col-xs-12">
+												<input type="text" name="email" placeholder="Your Email" class="require"
+													data-valid="email" data-error="Email should be valid.">
+											</div>
+											<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+												<input type="text" name="subject" placeholder="Subject" class="require">
+											</div>
+											<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+												<textarea rows="4" name="message" placeholder="Message"
+													class="require"></textarea>
+											</div>
+											<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+												<div class="response"></div>
+											</div>
+											<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+												<button type="button" class="prt_btn submitForm"
+													form-type="contact">submit</button>
+											</div>
+										</div>
+									</form>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="col-xl-6 col-lg-6 col-md-12 col-sm-12 col-xs-12">
+					<div class="prt_contact_map">
+						<iframe
+							src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3325.7149899484166!2d-92.46035748446522!3d33.534795052266986!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x862d89d84d550aef%3A0xc554ea1ecff51963!2s368+7th+%237%2C+Hampton%2C+AR+71744%2C+USA!5e0!3m2!1sen!2sin!4v1506497917270"
+							allowfullscreen></iframe>
+					</div>
+				</div>
+				<div class="prt_contact_details">
+					<div class="row">
+						<div class="col-lg-4 col-md-6 col-sm-6 col-xs-12">
+							<div class="prt_contact_details_box details_box1">
+								<h4>Phone</h4>
+								<p>+1-202-555-0125<br>01-202-555-0125</p>
+							</div>
+						</div>
+						<div class="col-lg-4 col-md-6 col-sm-6 col-xs-12">
+							<div class="prt_contact_details_box details_box2">
+								<h4>Email</h4>
+								<p><a href="#">name@yourmail.com</a><br><a href="#">mail@yourname.com</a></p>
+							</div>
+						</div>
+						<div class="col-lg-4 col-md-6 col-sm-6 col-xs-12">
+							<div class="prt_contact_details_box details_box3">
+								<h4>Address</h4>
+								<p>71 Pilgrim Avenue<br>Chevy Chase, MD 20815</p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="prt_footer_wrapper  ">
+			<div class="container">
+				<div class="row">
+					<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+						<p>Copyright © 2022 <a href="#">Portfolio</a>. All Rights Reserved</p>
+						<ul>
+							<li><a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
+							<li><a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
+							<li><a href="#"><i class="fa fa-pinterest-p" aria-hidden="true"></i></a></li>
+							<li><a href="#"><i class="fa fa-behance" aria-hidden="true"></i></a></li>
+							<li><a href="#"><i class="fa fa-google-plus" aria-hidden="true"></i></a></li>
+							<li><a href="#"><i class="fa fa-dribbble" aria-hidden="true"></i></a></li>
+						</ul>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!--Services Wrapper Start-->
+</body>
+
+</html>
---
+++ ejercicios diarios/plantilla/index.php
@@ -0,0 +1,219 @@
+<?php
+include_once 'encabezado.html';
+?>
+<body>
+	<!--Preloader Start-->
+	<div id="preloader">
+		<div id="status">
+			<img src="http://kamleshyadav.com/html/portfolio/html/bootstrap5/white_version_typed/http://kamleshyadav.com/html/portfolio/html/bootstrap5/white_version_typed/images/header/logo.png"
+				alt="loader">
+		</div>
+	</div>
+	<!--About Wrapper Start-->
+	<div class="prt_about_wrapperd">
+		<div class="prt_close_wrapper">
+			<img src="images/header/down_arrow.png" alt="Close" class="prt_close">
+		</div>
+		<div class="container">
+			<div class="prt_about_info ">
+				<div class="row">
+					<div class="col-lg-6 col-md-12 col-sm-12 col-xs-12">
+						<div class="prt_about_img">
+							<img src="https://avatars.githubusercontent.com/u/22733769?v=4" alt="About">
+						</div>
+					</div>
+					<div class="col-lg-6 col-md-12 col-sm-12 col-xs-12">
+						<div class="prt_heading_wrapper_2">
+							<div class="prt_heading ">
+								<h1>About ME</h1>
+								<div class="typed-strings">
+									<p>WHo AM i</p>
+									<p>Know more</p>
+								</div>
+								<p class="typed"></p>
+							</div>
+						</div>
+						<div class="prt_about_details">
+							<p>There are many variations of passages of Lorem Ipsum available, but the majority have
+								suffered alteration in some form, by injected humour, or randomised words which don't
+								look even slightly believable. If you are going to use a passage of Lorem Ipsum, you
+								need to be sure there isn't anything embarrassing hidden in the middle of text. All the
+								Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary,
+								making this the first true generator on the Internet. It uses a dictionary of over 200
+								Latin words, combined with a handful of model sentence structures.</p>
+							<a href="#" class="prt_btn">Download Resume</a> <a href="#" class="prt_btn">Hire Me</a>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="prt_about_edulearn_wrapper ">
+				<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+					<div class="prt_heading_wrapper">
+						<div class="prt_heading">
+							<h1>EDucation</h1>
+							<div class="typed-strings">
+								<p>learning</p>
+								<p>Reading</p>
+							</div>
+							<p class="typed"></p>
+						</div>
+					</div>
+					<div class="prt_about_learnsection">
+						<div class="row">
+							<div class="col-xl-6 offset-xl-6 col-md-12 offset-md-12 col-sm-12 col-xs-12">
+								<div class="prt_about_learnbox_right">
+									<div class="prt_about_learnbox_year">
+										<h1>2004</h1>
+									</div>
+									<div class="prt_about_learnbox_info">
+										<h4>Diploma In UI/UX Design</h4>
+										<span>New York</span>
+										<p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium
+											doloremque laudantium, totam rem aperiam.</p>
+									</div>
+								</div>
+							</div>
+							<div class="col-lg-6 col-md-12 col-sm-12 col-xs-12">
+								<div class="prt_about_learnbox_left">
+									<div class="prt_about_learnbox_year">
+										<h1>2006</h1>
+									</div>
+									<div class="prt_about_learnbox_info">
+										<h4>Diploma In Web Design</h4>
+										<span>New York</span>
+										<p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium
+											doloremque laudantium, totam rem aperiam.</p>
+									</div>
+								</div>
+							</div>
+							<div class="col-xl-6 offset-xl-6 col-md-12 offset-md-12 col-sm-12 col-xs-12">
+								<div class="prt_about_learnbox_right">
+									<div class="prt_about_learnbox_year">
+										<h1>2008</h1>
+									</div>
+									<div class="prt_about_learnbox_info">
+										<h4>Diploma In UI/UX Design</h4>
+										<span>New York</span>
+										<p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium
+											doloremque laudantium, totam rem aperiam.</p>
+									</div>
+								</div>
+							</div>
+							<div class="col-lg-6 col-md-12 col-sm-12 col-xs-12">
+								<div class="prt_about_learnbox_left">
+									<div class="prt_about_learnbox_year">
+										<h1>2010</h1>
+									</div>
+									<div class="prt_about_learnbox_info">
+										<h4>Diploma In Web Design</h4>
+										<span>New York</span>
+										<p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium
+											doloremque laudantium, totam rem aperiam.</p>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="prt_about_experience_wrapper ">
+				<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+					<div class="prt_heading_wrapper">
+						<div class="prt_heading">
+							<h1>Experience</h1>
+							<div class="typed-strings">
+								<p>involvement</p>
+								<p>participation</p>
+							</div>
+							<p class="typed"></p>
+						</div>
+					</div>
+					<div class="prt_about_experience">
+						<div class="row">
+							<div class="col-lg-6 col-md-12 col-sm-12 col-xs-12">
+								<div class="prt_about_experiencebox">
+									<div class="prt_about_experience_year">
+										<h1>2012</h1>
+										<h4>July to 2013 Sep</h4>
+									</div>
+									<div class="prt_about_experience_info">
+										<h4>Junior Ui/Ux Designer</h4>
+										<span>Amazon</span>
+										<p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium
+											doloremque laudantium, totam rem aperiam.</p>
+									</div>
+								</div>
+							</div>
+							<div class="col-lg-6 col-md-12 col-sm-12 col-xs-12">
+								<div class="prt_about_experiencebox">
+									<div class="prt_about_experience_year">
+										<h1>2013</h1>
+										<h4>Oct to 2014 Dec</h4>
+									</div>
+									<div class="prt_about_experience_info">
+										<h4>Senior Ui/Ux Designer</h4>
+										<span>Adobe</span>
+										<p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium
+											doloremque laudantium, totam rem aperiam.</p>
+									</div>
+								</div>
+							</div>
+							<div class="col-lg-6 col-md-12 col-sm-12 col-xs-12">
+								<div class="prt_about_experiencebox">
+									<div class="prt_about_experience_year">
+										<h1>2015</h1>
+										<h4>Jun to 2015 Mar</h4>
+									</div>
+									<div class="prt_about_experience_info">
+										<h4>Senior Ui/Ux Designer</h4>
+										<span>Google</span>
+										<p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium
+											doloremque laudantium, totam rem aperiam.</p>
+									</div>
+								</div>
+							</div>
+							<div class="col-lg-6 col-md-12 col-sm-12 col-xs-12">
+								<div class="prt_about_experiencebox">
+									<div class="prt_about_experience_year">
+										<h1>2016</h1>
+										<h4>Apr to Present</h4>
+									</div>
+									<div class="prt_about_experience_info">
+										<h4>Lead Ui/Ux Designer</h4>
+										<span>Google</span>
+										<p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium
+											doloremque laudantium, totam rem aperiam.</p>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<div class="prt_footer_wrapper  ">
+			<div class="container">
+				<div class="row">
+					<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+						<!-- <p>Copyright © 2023 <a href="#">Portfolio</a>. All Rights Reserved</p> -->
+						<p>Copyright © 2023 <a href="index.html">Inicio</a>. Todos los derechos reservados</p>
+						<ul>
+
+							Redes sociales:
+							<li><a href="#"><i class="fa fa-facebook" ></i></a></li>
+							<li><a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
+
+						</ul>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	</div>
+<?php
+include 'js.html';
+?>
+
+</html>
