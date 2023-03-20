7608a3b457913b856851d50e7d91bf4f42fcd544
diegoteos <diegoteos@hotmail.com>
2023-03-19 22:03:03 -0600

    captcha

---
+++ ejercicios diarios/captcha/captcha.php
@@ -0,0 +1,42 @@
+<?php
+
+$permitted_chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+ 
+function generate_string($input, $strength = 5) {
+    $input_length = strlen($input);
+    $random_string = '';
+    for($i = 0; $i < $strength; $i++) {
+        $random_character = $input[mt_rand(0, $input_length - 1)];
+        $random_string .= $random_character;
+    }
+ 
+    return $random_string;
+}
+
+$string_length = 6;
+$captcha_string = generate_string($permitted_chars, $string_length);
+
+
+$image = imagecreatetruecolor(200, 50);
+
+imageantialias($image, true);
+
+$colors = [];
+
+$red = rand(125, 175);
+$green = rand(125, 175);
+$blue = rand(125, 175);
+
+for($i = 0; $i < 5; $i++) {
+  $colors[] = imagecolorallocate($image, $red - 20*$i, $green - 20*$i, $blue - 20*$i);
+}
+
+imagefill($image, 0, 0, $colors[0]);
+
+for($i = 0; $i < 10; $i++) {
+  imagesetthickness($image, rand(2, 10));
+  $rect_color = $colors[rand(1, 4)];
+  imagerectangle($image, rand(-10, 190), rand(-10, 10), rand(-10, 190), rand(40, 60), $rect_color);
+}
+
+?>
