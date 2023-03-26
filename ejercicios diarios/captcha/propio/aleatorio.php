ed6ed98def2d162ce0e43d6c554c3c2afe44d71e
diegoteos <diegoteos@hotmail.com>
2023-03-25 23:56:08 -0600

    valor

--- ejercicios diarios/captcha/propio/aleatorio.php
+++ ejercicios diarios/captcha/propio/aleatorio.php
@@ -1,8 +1,8 @@
 <?php
 $permitted_chars = '0123456789abcdefghijklmnopqrstuvwxyz';
 // Output: 54esmdr0qf 
-echo substr(str_shuffle($permitted_chars), 0, 2);
-// $permitted_chars = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
-// // Output: video-g6swmAP8X5VG4jCi.mp4 
-// echo 'video-'.substr(str_shuffle($permitted_chars), 0, 16).'.mp4';
+$valor =  substr(str_shuffle($permitted_chars), 0, 5);
+
+session_start();
+$_SESSION["$valor"];
 ?>
