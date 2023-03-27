78026222d7754ddaf91a91054efc4a908fe5abc9
diegoteos <diegoteos@hotmail.com>
2023-03-26 22:32:15 -0600

    chaptcha con sessiones

--- ejercicios diarios/captcha/propio/aleatorio.php
+++ ejercicios diarios/captcha/propio/aleatorio.php
@@ -1,8 +1,48 @@
-<?php
-$permitted_chars = '0123456789abcdefghijklmnopqrstuvwxyz';
-// Output: 54esmdr0qf 
-$valor =  substr(str_shuffle($permitted_chars), 0, 5);
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="" method="post">
+        <input type="text" name="valor" id="">
+        <input type="submit" value="Enviar">
+    </form>
+</body>
+</html>
 
-session_start();
-$_SESSION["$valor"];
+<?php
+if (!$_POST){
+
+    $permitted_chars = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+
+    $codigo =  substr(str_shuffle($permitted_chars), 0, 5);
+    
+    session_start();
+    $captcha = $_SESSION['valor'] = $codigo;
+    echo $captcha;
+}
+
+
+
+
+if (!empty($_POST['valor'])){
+    session_start();
+    $valor = $_POST['valor'];
+    echo "<br>El valor recibido es: " . $valor;
+    if ($valor == $_SESSION['valor']) {
+        echo "<br>Si son iguales";
+        session_destroy();
+    } else{
+        echo "<br>Nada que ver";
+        session_destroy();
+    }
+}
+// if (!isset($_SESSION['id'])) {
+//     $_SESSION['id'] = $row['id'];
+//     header('Location: index.php');
+// }
 ?>
