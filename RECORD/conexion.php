0915e8191e164bbd483323d9aafb777b1c5ade75
diegoteos <diegoteos@hotmail.com>
2023-04-10 23:56:00 -0600

    guarda

---
+++ RECORD/conexion.php
@@ -0,0 +1,14 @@
+<?php
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "recordatorios";
+
+// Crea la conexión
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+
+// Verifica la conexión
+if (!$conn) {
+    die("Error de conexión: " . mysqli_connect_error());
+}
