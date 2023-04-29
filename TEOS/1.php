d773010c73cc00e0c6250e5b67dfe8efbddf8b39
diegoteos <diegoteos@hotmail.com>
2023-04-28 23:49:35 -0600

    apuntes

--- TEOS/1.php
+++ TEOS/1.php
@@ -1,4 +1,7 @@
-para empezar pensar
-todo se aprende menos a pensar
+para empezar, debo de pensar
+todo se aprende, no hay nada que no se haya inventado, todo se encuentra,
+lo que no se puede encontrar en internet es aprender a pensar
 debo recordar que la primera forma de hacer las cosas es la que a mi se me ocurre, no la que ya existe y mucho menos una mejor
-debo recordar que estoy fallando en algun aspecto menos imprtante pero que me hace no lograr lo que deseo hacer
+debo recordar que estoy fallando en algun aspecto de menor relevancia, importante es entonces, saber que me hace falta algo para lograr lo que deseo hacer ( esto es solo un poquito )
+Nunca, pero nunca olvidar este concepto importante ( yo soy una bestia, soy tonto por naturaleza, por lo tanto debo de volver a analizar todo lo que he hecho, todo lo que he pensado)
+Volver e iniciar de nuevo es una forma de mejorar...
