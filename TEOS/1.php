cbc61e2769b6e8b62a0b763ece49511dd0818ae5
diegoteos <diegoteos@hotmail.com>
2023-04-28 23:41:31 -0600

    mis apuntos

--- TEOS/1.php
+++ TEOS/1.php
@@ -1,2 +1,4 @@
 para empezar pensar
 todo se aprende menos a pensar
+debo recordar que la primera forma de hacer las cosas es la que a mi se me ocurre, no la que ya existe y mucho menos una mejor
+debo recordar que estoy fallando en algun aspecto menos imprtante pero que me hace no lograr lo que deseo hacer
