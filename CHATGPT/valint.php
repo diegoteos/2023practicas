72e223506a9d6e3b48513f3d72d4492fd5e95305
diegoteos <diegoteos@hotmail.com>
2023-05-10 22:56:13 -0600

    numeros

--- CHATGPT/valint.php
+++ CHATGPT/valint.php
@@ -4,3 +4,7 @@ $numero = "numero 2";
 $num = intval($numero);
 
 echo $num;
+
+$string = "La casa tiene 3 habitaciones y cuesta 200000 dólares.";
+preg_match_all('/\d+/', $string, $matches);
+print_r($matches[0]); // Imprime un array con los números encontrados
